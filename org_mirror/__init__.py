af33347187ff3ec36ece2f93d6e5881b68a8ecb2
Bartłomiej Rudecki <przekichane@icloud.com>
2024-03-09 20:34:00 +0100

    log to file

--- org_mirror/__init__.py
+++ org_mirror/__init__.py
@@ -3,7 +3,9 @@ import logging
 
 logging.basicConfig(
     format="%(asctime)s - %(name)s - %(levelname)s - %(message)s",
-    encoding="utf-8",
     level=logging.INFO,
+    handlers=[
+        logging.FileHandler(f"{__name__}.log", encoding="utf-8")
+    ]
 )
 log = logging.getLogger(__name__)
